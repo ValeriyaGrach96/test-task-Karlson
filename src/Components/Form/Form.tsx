68b0af88ce570ed342411f93825c71bbbce5900b
valeriyagracheva <valeria.gracheva@altenar.com>
2021-08-08 23:02:33 +0300

    fixed http API request

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -55,7 +55,7 @@ function Form(): JSX.Element {
   );
 
   const validateNumber = async (number: number): Promise<boolean> => {
-    const response =  await fetch(`http://apilayer.net/api/validate?access_key=b63d6b11c2871c38f0d17ff886cf438e&country_code=RU&number=${number}`)
+    const response =  await fetch(`https://apilayer.net/api/validate?access_key=b63d6b11c2871c38f0d17ff886cf438e&country_code=RU&number=${number}`)
     const json = await response.json();
     return Boolean(json.valid);
   }
@@ -86,7 +86,7 @@ function Form(): JSX.Element {
     },
     [handlePhoneNumber]
   );
-  
+
   const onSubmit = async () => {
     const isValid = await validateNumber(Number(phoneNumber));
     if(isValid) {
